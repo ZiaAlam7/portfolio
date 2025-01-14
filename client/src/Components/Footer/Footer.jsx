d920d2893358b7fc38960139654cb555c0ace7dc
ZiaAlam7 <ziaalam1446@gmail.com>
2025-01-14 13:19:29 +0500

    contact updated

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className='foot'>
       <div className="footer_social">
             <a href="mailto:ziaalam.dev@gmail.com" target="_blank" title="Email"><img src={gmail} alt="gmail icon" /></a>
-            <a href="https://wa.me/00923340921313" target="_blank" title="WhatsApp"><img src={whatsapp} alt="whatsapp icon" /></a>
+            <a href="https://wa.me/+923340921313" target="_blank" title="WhatsApp"><img src={whatsapp} alt="whatsapp icon" /></a>
             <a href="https://www.linkedin.com/in/zia-alam-102572227/" target="_blank" title="LinkedIn Profile"><img src={linkedin} alt="linkedin icon" /></a>
             <a href="https://github.com/ZiaAlam7" target="_blank" title="GitHub Profile"><img src={github} alt="github icon" /></a>
       </div>
