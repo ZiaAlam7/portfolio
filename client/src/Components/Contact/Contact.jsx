900fc1d944a472b6dadc13a0491b7da19e76499f
Zia Alam <ziaalam1446@gmail.com>
2025-01-14 12:12:31 +0500

    Update Contact.jsx

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = ({ scrollPosition }) => {
     e.preventDefault();
     setStatus('Sending...');
 
-    const response = await fetch('http://localhost:3001/send-email', {
+    const response = await fetch('https://ziaalam-portfolio-server.vercel.app/send-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
